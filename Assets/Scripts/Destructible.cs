75c839df84358e0a6854a1f481cb010163f897cc
wolwer1ne <wolwerine@yandex.ru>
2019-12-10 15:00:14 +0300

    Starts destructible

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Destructible : MonoBehaviour
+{
+    public GameObject particle;
+
+    public float maxRotation;
+    // Start is called before the first frame update
+    private List<(Vector3, Quaternion)> _particlesPositions;
+    void Start()
+    {
+     
+        _particlesPositions = new List<(Vector3, Quaternion)>();
+        var bounds = GetComponent<Renderer>().bounds;
+        var size = bounds.size;
+        var particleSize = particle.GetComponent<Renderer>().bounds.size;
+        var xScale = Math.Round(size.x / particleSize.x);
+        var yScale = Math.Round(size.y / particleSize.y);
+        var zScale = Math.Round(size.z / particleSize.z);
+        for (var i = 0; i < xScale; i++)
+        for (var j = 0; j < yScale; j++)
+        for (var k = 0; k < zScale; k++)
+        {
+            _particlesPositions.Add((new Vector3( i * particleSize.x,
+                 j * particleSize.y,
+                k * particleSize.z),   
+                Quaternion.Euler(Random.Range(-maxRotation,maxRotation), 
+                Random.Range(-maxRotation, maxRotation), 
+                Random.Range(-maxRotation, maxRotation))));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Destructible"))
+        {
+            var currentObject = gameObject;
+            foreach (var (position, rotation) in _particlesPositions)
+            {
+               var newParticle = Instantiate(particle);
+               newParticle.transform.localPosition = position;
+               newParticle.transform.position += currentObject.transform.position;
+               newParticle.transform.rotation = rotation;
+            }
+          
+            gameObject.SetActive(false);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public GameObject target;
+    private bool _hasTarget = false;
+    private Vector3 _targetPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (target != null)
+        {
+            var position = target.transform.position;
+            _targetPosition = new Vector3(position.x, position.y, 
+                position.z);
+            _hasTarget = true;
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_hasTarget)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _targetPosition, Time.deltaTime);  
+
+        }
+    }
+}
